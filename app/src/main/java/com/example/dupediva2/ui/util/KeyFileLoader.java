1b0bef8d2f9a0e85054ad177598513b7fd7d5a3e
Rhea Modey <rmodey2@ncsu.edu>
2024-02-24 15:30:54 -0500

    [keyfile changes]

---
+++ app/src/main/java/com/example/dupediva2/ui/util/KeyFileLoader.java
@@ -0,0 +1,15 @@
+package com.example.dupediva2.ui.util;
+
+import android.content.Context;
+import java.io.InputStream;
+import com.example.dupediva2.R;
+
+
+public class KeyFileLoader {
+
+    private static final int resourceId = R.raw.quick_ray_415317_591dcd4dccdb;
+
+    public static InputStream loadKeyFileInputStream(Context context) {
+        return context.getResources().openRawResource(resourceId);
+    }
+}
