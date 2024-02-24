4f6b59d86b6922e06172240da6db0dff2e35f61e
phpatel6 <phpatel6@ncsu.edu>
2024-02-24 17:38:07 -0500

    "COMMIT"

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -44,4 +44,5 @@ dependencies {
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
     implementation ("com.google.cloud:google-cloud-vision:3.34.0")
+
 }
