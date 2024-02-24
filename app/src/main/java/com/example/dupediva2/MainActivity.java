7272597f9a88c4ceb573007d80a8382dfa361aad
pathiruk <pathiruk@ncsu.edu>
2024-02-24 13:20:27 -0500

    added login and camera buttons functionality

--- app/src/main/java/com/example/dupediva2/MainActivity.java
+++ app/src/main/java/com/example/dupediva2/MainActivity.java
@@ -1,14 +1,19 @@
 package com.example.dupediva2;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
 import android.graphics.Color;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 
-
+import com.example.dupediva2.ui.login.LoginActivity;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -40,15 +45,38 @@ public class MainActivity extends AppCompatActivity {
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
 
 
-
         final Button loginButton = findViewById(R.id.Login);
         loginButton.setBackgroundColor(Color.parseColor("#F8C7C6"));
 
         loginButton.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
+                openLoginActivity();
+            }
+        });
+
+
+        final ImageButton camera = findViewById(R.id.camera);
+        camera.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                startActivity(intent);
 
             }
         });
+
+        final ImageButton upload = findViewById(R.id.upload);
+        upload.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                startActivity(intent);
+
+            }
+        });
+    }
+
+    private void openLoginActivity() {
+        Intent intent = new Intent(this, LoginActivity.class);
+        startActivity(intent);
     }
 
 
